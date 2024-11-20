e90a4a9dbcd7f1c80ecb267ebf5c49944361e569
RamAvni <ramavni@hilma.tech>
2024-11-20 20:44:04 +0200

    can see files on screen

--- client/src/pages/User.jsx
+++ client/src/pages/User.jsx
@@ -1,3 +1,21 @@
-function User() {}
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function User() {
+    const [files, setFiles] = useState();
+    let { username } = useParams();
+
+    useEffect(() => {
+        postInfoToServer(username);
+    }, []);
+
+    async function postInfoToServer(username) {
+        const res = await fetch(`http://localhost:3000/users/${username}`);
+        const data = await res.json();
+        console.log(data, res);
+        setFiles(data);
+    }
+    return <h1>{`${files}`}</h1>;
+}
 
 export default User;
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,11 +2,13 @@ var express = require("express");
 var router = express.Router();
 var app = express();
 const path = require("path");
+const fs = require("fs");
 
-async function getFileNames(username) {
-    const res = await fs.readdir(path.join(__dirname, `../db/userfiles/${username}`));
-    const data = res.json();
-    return data;
+function getFileNames(username) {
+    const res = fs.readdirSync(path.join(__dirname, `../db/userfiles/${username}`));
+    console.log(res);
+
+    return res;
 }
 
 const options = {
@@ -15,7 +17,7 @@ const options = {
 
 /* GET users listing. */
 router.get("/:username", function (req, res, next) {
-    res.send(getFileNames(req.params.username));
+    res.send(JSON.stringify(getFileNames(req.params.username)));
 });
 
 router.post("/", (req, res) => {
