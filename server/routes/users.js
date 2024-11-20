7d7dd683630ded88f11136de235c249650fce62d
RamAvni <ramavni@hilma.tech>
2024-11-20 20:10:50 +0200

    eefe

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,19 +1,22 @@
 var express = require("express");
 var router = express.Router();
-var app = express()
-const path = require('path')
-//
+var app = express();
+const path = require("path");
+
+async function getFileNames(username) {
+    const res = await fs.readdir(path.join(__dirname, `../db/userfiles/${username}`));
+    const data = res.json();
+    return data;
+}
+
 const options = {
-    root: path.join(__dirname,'..')
+    root: path.join(__dirname, ".."),
 };
 
-
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-    res.send("respond with a resource");  
+router.get("/:username", function (req, res, next) {
+    res.send(getFileNames(req.params.username));
 });
-
-
 
 router.post("/", (req, res) => {
     console.log(req.body);
