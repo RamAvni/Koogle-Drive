213f979f5b4f5c61ea65c1ed8cbdc9094c2f1e9b
RamAvni <ramavni@hilma.tech>
2024-11-20 16:51:43 +0200

    WORKING REGISTER

--- server/routes/register.js
+++ server/routes/register.js
@@ -6,34 +6,31 @@ const path = require("path");
 // import {firstChecks} from "../public/utilities"
 const PathToDb = path.join(__dirname, "../db/users.json");
 
-let uniqueId = 1;
-const updateId = () => {
-  uniqueId++;
-};
-
 router.post("/", (req, res) => {
-  console.log("req.body", req.body);
-  let username = req.body.username;
-  let password = req.body.password;
-  let user = {
-    username: username,
-    password: password,
-    id: uniqueId,
-  };
-  updateId();
-  // let check1 = firstChecks(username, password)
-  if (typeof username === "string" && username.length > 0 && password.length > 0) {
-    console.log("first check passed");
-    let usersjson = fs.readFileSync(PathToDb, "utf-8");
-    console.log("usersjson: ", usersjson);
-    let users = JSON.parse(usersjson);
-    users.push(user);
-    usersjson = JSON.stringify(users);
-    fs.writeFileSync(PathToDb, usersjson);
-    console.log("i passed the commiting code");
-  }
+    console.log("req.body", req.body);
+    let username = req.body.username;
+    let password = req.body.password;
+    let user = {
+        username: username,
+        password: password,
+    };
 
-  res.send(JSON.stringify(user));
+    // let check1 = firstChecks(username, password)
+    if (typeof username === "string" && username.length > 0 && password.length > 0) {
+        console.log("first check passed");
+        let usersjson = fs.readFileSync(PathToDb, "utf-8");
+        console.log("usersjson: ", usersjson);
+        let users = JSON.parse(usersjson);
+        let newId = users[users.length - 1].id + 1;
+        console.log(users[users.length - 1].id);
+        user.id = newId;
+        users.push(user);
+        usersjson = JSON.stringify(users);
+        fs.writeFileSync(PathToDb, usersjson);
+        console.log("i passed the commiting code");
+    }
+
+    res.send(JSON.stringify(user));
 });
 
 module.exports = router;
