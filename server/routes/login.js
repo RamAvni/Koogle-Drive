d6d6510f07d44051145ff5f2fd5514ded0bc993d
naamaWyler <naamawyler@hilma.tech>
2024-11-20 20:10:18 +0200

    message

--- server/routes/login.js
+++ server/routes/login.js
@@ -8,23 +8,23 @@ router.post("/", (req, res) => {
   let username = req.body.username;
   let password = req.body.password;
   let correctUser;
+
   if (typeof username === "string" && username.length > 0 && password.length > 0) {
     let usersjson = fs.readFileSync(PathToDb, "utf-8");
     let users = JSON.parse(usersjson);
 
-    doesUserAlreadyExists = users.filter((u) => username === u.username && password === u.password);
-    if (doesUserAlreadyExists.length === 0) {
+    correctUser = users.filter((u) => username === u.username && password === u.password);
+    if (correctUser.length === 0) {
       return res.status(401).json({
         message: "username or password incorrect",
       });
     }
-
-    correctUser = users.filter((u) => username === u.username && password === u.password);
   }
 
   res.send(
     JSON.stringify({
       username: username,
+      id: correctUser[0].id,
     })
   );
 });
