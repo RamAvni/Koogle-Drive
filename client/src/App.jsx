8648266116387290ccc571a9c962688795186589
naamaWyler <naamawyler@hilma.tech>
2024-11-20 14:53:23 +0200

    accidentally messed with server, but added db

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,36 +2,61 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-    const [data, setData] = useState("");
-    async function server() {
-        const res = await fetch("http://localhost:3000/users", {
-            method: "POST",
-            body: JSON.stringify({
-                userId: 1,
-                title: "Demo Todo Data",
-                completed: false,
-            }),
-            headers: {
-                "Content-type": "application/json",
-            },
+  const [data, setData] = useState("");
+  async function server() {
+    const res = await fetch("http://localhost:3000/users", {
+      method: "POST",
+      body: JSON.stringify({
+        userId: 1,
+        title: "Demo Todo Data",
+        completed: false,
+      }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    console.log("res: ", res);
+
+    const data = await res.json();
+    console.log("data:", data);
+    setData(JSON.stringify(data));
+  }
+
+  useEffect(() => {
+    server();
+  }, []);
+  async function post() {
+    const get = await fetch("http://localhost:3000/users", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        // Check if the response is okay
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json(); // Parse the JSON data
+      })
+      .then((data) => {
+        console.log("Fetched users:", data); // Handle the data
+        data.forEach((user) => {
+          console.log(`User ID: ${user.id}, Name: ${user.name}`);
         });
-        console.log("res: ", res);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error); // Handle errors
+      });
+  }
 
-        const data = await res.json();
-        console.log("data:", data);
-        setData(JSON.stringify(data));
-    }
+  return (
+    <>
+      <h1>Always Appears</h1>
 
-    useEffect(() => {
-        server();
-    }, []);
-    return (
-        <>
-            <h1>Always Appears</h1>
-
-            <p>{`${data}`}</p>
-        </>
-    );
+      <p>{`${data}`}</p>
+    </>
+  );
 }
 
 export default App;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,19 @@
 var express = require("express");
 var router = express.Router();
+var app = express()
+
+//
+const options = {
+    root: path.join(__dirname,'..')
+};
+
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-    res.send("respond with a resource");
+    res.send("respond with a resource");  
 });
+
+
 
 router.post("/", (req, res) => {
     console.log(req.body);
