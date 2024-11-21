8f9dc62072d12872ee7e4e93f2c0fa1960181a5c
naamaWyler <naamawyler@hilma.tech>
2024-11-21 08:29:17 +0200

    redirecting after login and Register

--- client/src/pages/User.jsx
+++ client/src/pages/User.jsx
@@ -2,20 +2,19 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function User() {
-    const [files, setFiles] = useState();
-    let { username } = useParams();
+  const [files, setFiles] = useState();
+  let { username } = useParams();
 
-    useEffect(() => {
-        postInfoToServer(username);
-    }, []);
+  useEffect(() => {
+    postInfoToServer(username);
+  }, []);
 
-    async function postInfoToServer(username) {
-        const res = await fetch(`http://localhost:3000/users/${username}`);
-        const data = await res.json();
-        console.log(data, res);
-        setFiles(data);
-    }
-    return <h1>{`${files}`}</h1>;
+  async function postInfoToServer(username) {
+    const res = await fetch(`http://localhost:3000/users/${username}`);
+    const data = await res.json();
+    setFiles(data);
+  }
+  return <h1>{`${files}`}</h1>;
 }
 
 export default User;
